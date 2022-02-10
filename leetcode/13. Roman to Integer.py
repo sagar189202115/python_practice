ecd79dac7cfb3bbe73043ca176a9200a7afd399a
sagar verma <sagar189202115@gmail.com>
2022-02-10 20:09:20 +0530

    roman to integer

---
+++ leetcode/13. Roman to Integer.py
@@ -0,0 +1,15 @@
+s='XIV'
+tonum=0
+d={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+
+j=0
+k=d[s[-1]]
+su=0
+for i in s[::-1]:
+
+    if d[i]<k:
+        su-=d[i]
+    else:
+        su+=d[i]
+        k=d[i]
+print(su)
