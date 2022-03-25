988b154233c679a5e8b7851b40f0de9bc4621152
sagar verma <sagar189202115@gmail.com>
2022-03-25 20:42:55 +0530

    new problems solved

---
+++ leetcode/162. Find Peak Element.py
@@ -0,0 +1,18 @@
+import math
+
+
+def peakelement(nums):
+    maxx=0
+    nums.append(-math.inf)
+    i=0
+    while i<len(nums):
+        if nums[i]>nums[maxx]:
+            if nums[i+1]<nums[i]>nums[i-1]:
+                maxx=i if nums[i]>nums[maxx] else None
+
+            if nums[i+1]>nums[i]:
+                i+=1
+            else:i+=2
+        else:i+=1
+    return maxx
+print(peakelement([1,2,1,3,5,6,4]))
