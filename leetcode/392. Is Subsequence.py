e823bf67181fb58fcdd2d00015f5b2f8a9dd71ce
sagar verma <sagar189202115@gmail.com>
2022-03-28 00:05:20 +0530

    new problems solved

---
+++ leetcode/392. Is Subsequence.py
@@ -0,0 +1,12 @@
+def issubsequence(s,t):
+    if s == '': return True
+    i = 0
+    j = 0
+    while i < len(s) and j < len(t):
+        if s[i] == t[j]:
+            i += 1
+            j += 1
+        else:
+            j += 1
+    return i == len(s)
+print(issubsequence('abc','ahbgdc'))
