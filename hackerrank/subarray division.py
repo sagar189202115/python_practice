b447a9ac6048f4f7ef96b1aa96cd548da401959f
sagar verma <sagar189202115@gmail.com>
2022-04-04 17:32:22 +0530

    subarray divisiion

--- hackerrank/subarray division.py
+++ hackerrank/subarray division.py
@@ -1,1 +1,15 @@
-a=[3,4,3,1,2,5,6,0]
+def subarraydivision(s,d,m):
+    i = 0
+    t = 0
+    c = 0
+    for j in range(len(s)):
+        t += s[j]
+        if j >= m - 1:
+
+            if t == d:
+                c += 1
+            t -= s[i]
+            i += 1
+        j += 1
+    return c
+print(subarraydivision([1,2,1,3,2],3,2))
