99f5b9ab242b58ec3eef3539411d23fe9b682bb6
sagar verma <sagar189202115@gmail.com>
2022-04-17 23:40:06 +0530

    increasing order search tree

--- leetcode/897. Increasing Order Search Tree.py
+++ leetcode/897. Increasing Order Search Tree.py
@@ -7,16 +7,13 @@ class Solution:
     def increasingBST(self, root: TreeNode) -> TreeNode:
         self.res = TreeNode(0)
         self.re = self.res
-
         def bst(r):
             if r:
                 bst(r.left)
                 self.res.right = TreeNode(r.val)
                 self.res = self.res.right
                 print(self.res)
-
                 bst(r.right)
-
         bst(root)
         return self.re.right
 
